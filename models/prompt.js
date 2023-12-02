589a5c6cff5781019741d197c9fbd7fad1a7eb70
Jigar Patel <jpxtel98@outlook.com>
2023-12-01 22:26:51 -0500

    Set up prompt model for mongodb

---
+++ models/prompt.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const promptSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    tags: [String], // Array of strings for categorization
+    likes: {
+        type: Number,
+        default: 0 // To track user likes or upvotes
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Prompt', promptSchema);
+
