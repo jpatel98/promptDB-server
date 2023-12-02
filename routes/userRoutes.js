3ab559369413d6639824ac86cdbcbc674fc289d5
Jigar Patel <jpxtel98@outlook.com>
2023-12-01 22:41:15 -0500

    add userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// User registration
+router.post('/register', userController.register);
+
+// User login
+router.post('/login', userController.login);
+
+// Additional methods tbd
+
+module.exports = router;
