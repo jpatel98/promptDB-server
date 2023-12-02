e95fdb730c4c0c0385c43f805e4fdbc04ac24a76
Jigar Patel <jpxtel98@outlook.com>
2023-12-01 22:36:00 -0500

    Add more methods to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,71 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userController = {
-    // Register a new user
-    async register(req, res) {
-        try {
-            const { username, email, password } = req.body;
+  // Register a new user
+  async register(req, res) {
+    try {
+      const { username, email, password } = req.body;
 
-            // Check if user already exists
-            let user = await User.findOne({ email });
-            if (user) return res.status(400).send('User already exists.');
+      // Validate input (you can expand this with more sophisticated validation)
+      if (!username || !email || !password) {
+        return res.status(400).send("All fields are required");
+      }
 
-            // Create a new user
-            user = new User({ username, email, password });
-            await user.save();
+      // Check if user already exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).send("User already exists.");
+      }
 
-            // Generate a token
-            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+      // Create a new user
+      user = new User({
+        username,
+        email,
+        password: await bcrypt.hash(password, 10),
+      });
+      await user.save();
 
-            res.status(201).send({ token });
-        } catch (error) {
-            res.status(500).send('Error in saving user');
-        }
-    },
+      // Generate a token
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
 
-    // User login
-    async login(req, res) {
-        try {
-            const { email, password } = req.body;
-            const user = await User.findOne({ email });
+      // Return the token and user details (excluding password)
+      res.status(201).send({ user: { username, email }, token });
+    } catch (error) {
+      res.status(500).send("Error in saving user: " + error.message);
+    }
+  },
 
-            if (!user || !await bcrypt.compare(password, user.password)) {
-                return res.status(400).send('Invalid credentials');
-            }
+  // User login
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-            res.send({ token });
-        } catch (error) {
-            res.status(500).send('Error in user login');
-        }
-    },
+      // Input validation
+      if (!email || !password) {
+        return res.status(400).send("Email and password are required");
+      }
 
-    // Additional methods to be added
+      // Check if user exists
+      const user = await User.findOne({ email });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send("Invalid credentials");
+      }
+
+      // Generate a token
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    } catch (error) {
+      res.status(500).send("Error in user login: " + error.message);
+    }
+  },
+
+  // Additional methods tbd
 };
 
 module.exports = userController;
